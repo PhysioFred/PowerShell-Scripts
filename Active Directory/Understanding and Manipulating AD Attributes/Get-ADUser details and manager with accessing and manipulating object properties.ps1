ec098f6d92df1a43cd7335e78c81437a295f8516
PhysioFred <codespace3166@gmail.com>
2024-09-10 07:50:41 +1000

    Create Get-ADUser details and manager with accessing and manipulating object properties.ps1
    add new parameter and move script to different folder

---
+++ Active Directory/Understanding and Manipulating AD Attributes/Get-ADUser details and manager with accessing and manipulating object properties.ps1
@@ -0,0 +1,8 @@
+﻿# Get the user with their manager's DN
+$user = Get-ADUser -Filter { name -like "Thor Odi*" } -Properties Manager
+
+# Get the manager's details using their DN
+$manager = Get-ADUser -Identity $user.Manager
+
+# Display the manager's name and SAMAccountName
+$manager | Select-Object Name, SAMAccountName, DistinguishedName
