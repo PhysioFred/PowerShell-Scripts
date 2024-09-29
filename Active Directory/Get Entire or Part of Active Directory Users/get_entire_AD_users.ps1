09facca3fa3f94834bc2890d8a7c74935eefaa0c
PhysioFred <codespace3166@gmail.com>
2024-09-29 20:59:11 +1000

    Learning Powershell with CSV files

---
+++ Active Directory/Get Entire or Part of Active Directory Users/get_entire_AD_users.ps1
@@ -0,0 +1,1 @@
+﻿get-aduser -filter * -Properties title, manager, description| export-csv -Path "C:\Users\Fred\PowerShell Scripts\Active Directory\Excel Files\List of users in OU _USERS\entireADdirectory2.csv"
