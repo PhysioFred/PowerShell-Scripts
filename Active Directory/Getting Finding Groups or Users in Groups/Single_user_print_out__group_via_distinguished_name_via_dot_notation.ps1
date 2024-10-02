3846999f8719396aee588dcb3c3731063207b6bd
PhysioFred <codespace3166@gmail.com>
2024-10-02 17:52:28 +1000

    finding full length ad groups via dot notation

---
+++ Active Directory/Getting Finding Groups or Users in Groups/Single_user_print_out__group_via_distinguished_name_via_dot_notation.ps1
@@ -0,0 +1,9 @@
+﻿# Query the group memberships for a single user (Thor)
+$user = "thor"
+
+# Get the user's details, including the MemberOf property
+$userDetails = Get-ADUser $user -Properties MemberOf
+
+# Display all groups the user belongs to by expanding the MemberOf property and printing the Distinguished Name (DN)
+Write-Host "Distinguished Names of Groups for user $($user):"
+$userDetails.MemberOf 
---
+++ Active Directory/Getting Finding Groups or Users in Groups/Single_user_print_out_group_via_human_readable_name.ps1
@@ -0,0 +1,9 @@
+﻿# Query the group memberships for a single user (Thor)
+$user = "thor"
+
+# Get the user's details, including the MemberOf property
+$userDetails = Get-ADUser $user -Properties MemberOf
+
+# Display all groups the user belongs to by expanding the MemberOf property
+Write-Host "Groups for user $($user):"
+$userDetails.MemberOf | ForEach-Object { Get-ADGroup $_ | Select-Object -ExpandProperty Name }
